4228e618ed15bc7ca759b4f19ca554c8dab2efbf
detlafff <52668473+detlafff@users.noreply.github.com>
2019-07-12 00:25:27 +0300

    -

---
+++ CnbApiClient/CnbService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using RestSharp;
+
+namespace CnbApiClient
+{
+    public class CnbService 
+    {
+        private IRestClient restClient;
+        private static string YearExchangeSegmentUrl = "year.txt";
+
+        public CnbService(Uri uri)
+        {
+            restClient = new RestClient(uri);
+        }
+
+        public string[] GetRawCurrecyByYear(int year)
+        {
+            var request = new RestRequest(YearExchangeSegmentUrl)
+            {
+                Method = Method.GET
+            };
+
+            request.AddParameter("year", year, ParameterType.QueryString);
+
+            var response = restClient.Execute(request);
+
+            return response.Content.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
---
+++ ExchngeLoaderConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExchngeLoaderConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var servise = new CnbApiClient.CnbService(
+                new Uri("https://www.cnb.cz/en/financial_markets/foreign_exchange_market/exchange_rate_fixing"));
+            var ans = servise.GetRawCurrecyByYear(2018);
+            Console.WriteLine(ans);
+            Console.ReadLine();
+        }
+    }
+}
